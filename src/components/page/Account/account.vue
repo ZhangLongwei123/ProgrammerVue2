a75707c7b88f44d174473304edf1047f28592edb
ZhangLongwei <2412426728@qq.com>
2020-08-27 10:52:42 +0800

    更新like1

--- src/components/page/Account/account.vue
+++ src/components/page/Account/account.vue
@@ -9,10 +9,13 @@
         </div>
         <div class="container">
             <div class="handle-box">
-
-                <el-button type="primary" @click="visible = true">添加课程类型</el-button>
-                <el-input v-model="query.name" placeholder="用户名" class="handle-input mr10"></el-input>
-                <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
+                <el-input v-model="query.username" placeholder="用户名" class="handle-input mr10"></el-input>
+                <el-input v-model="query.phone" placeholder="手机号" class="handle-input mr10"></el-input>
+                <el-input v-model="query.true_name" placeholder="真实姓名" class="handle-input mr10"></el-input>
+                <el-input v-model="query.email" placeholder="邮箱" class="handle-input mr10"></el-input>
+                <el-checkbox label="讲师" v-model="query.is_teacher"></el-checkbox>
+                <el-checkbox label="用户" v-model="query.is_user"></el-checkbox>
+                <el-button type="primary" style="margin-left: 20px;" icon="el-icon-search" @click="handleSearch">搜索</el-button>
             </div>
             <el-table
                     :data="nowTableData"
@@ -38,12 +41,12 @@
                         <el-button
                                 type="text"
                                 icon="el-icon-edit"
-                                @click="handleEdit(scope.row)"
+                                @click="disableAccount(scope.row)"
                         >禁用</el-button>
                         <el-button
                                 type="text"
                                 icon="el-icon-delete"
-                                @click="handleDel(scope.row)"
+                                @click="enableAccount(scope.row)"
                         >解封</el-button>
                     </template>
                 </el-table-column>
@@ -60,23 +63,6 @@
             </div>
         </div>
 
-        <!-- 编辑弹出框 -->
-        <el-dialog title="" :visible.sync="editVisible" width="30%">
-            <el-form ref="form" label-width="100px">
-                <el-form-item label="封禁原因">
-                    <el-input
-                            type="textarea"
-                            :autosize="{ minRows: 3, maxRows: 5}"
-                            placeholder="请输入封禁原因"
-                            v-model="baninfo">
-                    </el-input>
-                </el-form-item>
-            </el-form>
-            <span slot="footer" class="dialog-footer">
-                <el-button @click="editVisible = false">取 消</el-button>
-                <el-button type="primary" @click="saveEdit">确 定</el-button>
-            </span>
-        </el-dialog>
     </div>
 </template>
 <script>
@@ -92,12 +78,16 @@
         name: 'basetable',
         data() {
             return {
-                baninfo: '',
+                editDname: '',
                 query: {
-                    address: '',
-                    name: '',
                     pageIndex: 1,
-                    pageSize: 5
+                    pageSize: 5,
+                    username:'',
+                    phone:'',
+                    true_name:'',
+                    email:'',
+                    is_teacher:'',
+                    is_user:''
                 },
                 tableData: [],
                 multipleSelection: [],
@@ -128,29 +118,53 @@
             // 触发搜索按钮
             handleSearch() {
                 this.$set(this.query, 'pageIndex', 1);
-                this.getData();
+                let ist = -1;
+                if (this.query.is_teacher==true && this.query.is_user==""){
+                    ist = 1;
+                }else if (this.query.is_teacher=="" && this.query.is_user==true){
+                    ist = 0;
+                }
+                this.$axios2.post('UserAccountController/queryByLike',{username:this.query.username,phone:this.query.phone, true_name:this.query.true_name, email:this.query.email,is_teacher:ist}).then(res => {
+                    this.tableData = res;
+
+                }).catch(err=>console.log(err));
+                // this.getData();
             },
             // 多选操作
             handleSelectionChange(val) {
                 this.multipleSelection = val;
             },
             // 编辑操作
-            handleEdit(row) {
-                this.idx = row.t_user_id;
-                this.editVisible = true;
-            },
-            // 保存编辑ban_teacher:0,bandate:new Date(),ban_result:this.baninfo
-            saveEdit() {
-                this.$axios2.post('/UserAccountController/EditUserAccountPwdOrIsenable', {t_user_id:this.idx,isenabled:0}).then(res => {
-                    if(res == 1){
-                        this.$message.success("禁用成功")
-                    }else{
-                        this.$message.warning(("操作失败"))
+            disableAccount(row) {
+                if(row.isenabled==0){
+                    this.$message({ duration:1500,message:"已经禁用了呢",type:"info" });
+                    return;
+                }
+                // TODO 弹框确认
+                this.$axios2.post('UserAccountController/EditUserAccountPwdOrIsenable',{t_user_id:row.t_user_id,isenabled:0}).then(data2=>{
+                    if (data2==1){
+                        this.$message({ duration:1500,message:"禁用成功",type:"success" });
+                    }else {
+                        this.$message({ duration:1500,message:"操作失败",type:"warning" });
                     }
-                }).catch(err => {
-                    console.log(err)
-                })
-
+                    this.getData();
+                }).catch(err=>console.log(err));
+            },
+            // 保存编辑
+            enableAccount(row) {
+                if(row.isenabled==1){
+                    this.$message({ duration:1500,message:"已经解封了呢",type:"info" });
+                    return;
+                }
+                // TODO 弹框确认
+                this.$axios2.post('UserAccountController/EditUserAccountPwdOrIsenable',{t_user_id:row.t_user_id,isenabled:1}).then(data2=>{
+                    if (data2==1){
+                        this.$message({ duration:1500,message:"解封成功",type:"success" });
+                    }else {
+                        this.$message({ duration:1500,message:"操作失败",type:"warning" });
+                    }
+                    this.getData();
+                }).catch(err=>console.log(err));
             },
             // 分页导航
             handlePageChange(val) {
@@ -164,6 +178,7 @@
 <style scoped>
     .handle-box {
         margin-bottom: 20px;
+        margin-left: 20px;
     }
 
     .handle-select {
@@ -171,7 +186,7 @@
     }
 
     .handle-input {
-        width: 300px;
+        width: 200px;
         display: inline-block;
     }
     .table {

ffdeb41d1fb340d0e5104cc380e59a7c4ad69264
ZhangLongwei123 <2412426728@qq.com>
2020-08-29 11:14:14 +0800

    更新coursereport1

--- src/components/page/dept/position.vue
+++ src/components/page/dept/position.vue
@@ -2,9 +2,18 @@
     <div>
         <div id="header">
             <div class="handle-box">
-                <el-input v-model="deptName" placeholder="新部门名称" class="handle-input mr10"></el-input>
-                <el-input v-model="deptName" placeholder="新部门名称" class="handle-input mr10"></el-input>
-                <el-button type="primary" icon="el-icon-commit" @click="addDept">添加</el-button>
+                <el-button type="primary" @click="visible = true">添加职位</el-button>
+                <template>
+                    <el-select v-model="deptValue" placeholder="请选择部门">
+                        <el-option
+                                v-for="item in deptData"
+                                :label="item.dname"
+                                :value="item.did">
+                        </el-option>
+                    </el-select>
+                </template>
+                <el-input v-model="PositionName" placeholder="职位名称片段" class="handle-input mr10"></el-input>
+                <el-button type="primary" icon="el-icon-commit" @click="getData()">搜索</el-button>
             </div>
         </div>
         <el-table
@@ -15,11 +24,9 @@
                 header-cell-class-name="table-header"
         >
             <el-table-column type="selection" width="55" align="center"></el-table-column>
-            <el-table-column prop="did" label="编号" width="55" align="center"></el-table-column>
-            <el-table-column prop="dname" label="部门名称"></el-table-column>
-            <el-table-column prop="dname" label="部门名称"></el-table-column>
-            <el-table-column prop="dname" label="部门名称"></el-table-column>
-            <el-table-column prop="dname" label="部门名称"></el-table-column>
+            <el-table-column prop="pid" label="编号" width="55" align="center"></el-table-column>
+            <el-table-column prop="pname" label="职位名称"></el-table-column>
+            <el-table-column prop="did" label="部门"></el-table-column>
 
             <el-table-column label="操作" width="180" align="center">
                 <template slot-scope="scope">
@@ -46,15 +53,15 @@
                     @current-change="handlePageChange"
             ></el-pagination>
         </div>
-        <el-dialog v-dialogDrag title="部门名称修改" center :visible.sync="editVisible" width="30%">
+        <el-dialog v-dialogDrag title="职位添加" center :visible.sync="addVisible" width="30%">
             <el-form ref="form" label-width="150px">
-                <el-form-item label="课程类型名">
+                <el-form-item label="职位名">
                     <el-input v-model="editDept.did" readonly style="width: 200px;"></el-input>
                     <el-input v-model="editDept.dname" style="width: 200px;"></el-input>
                 </el-form-item>
             </el-form>
             <span slot="footer" class="dialog-footer">
-                <el-button @click="visible = false">取 消</el-button>
+                <el-button @click="addVisible = false">取 消</el-button>
                 <el-button type="primary" @click="eidtDeptName">确 定</el-button>
             </span>
         </el-dialog>
@@ -67,22 +74,28 @@
         data:function(){
             return {
                 tableData:[],
-                deptName:'',
                 query: {
                     address: '',
                     name: '',
                     pageIndex: 1,
                     pageSize: 5
                 },
-                editVisible: false,
+                addVisible: false,
                 editDept:{
                     did:'',
                     dname:''
+                },
+                deptData:[],
+                PositionName:'',
+                deptValue:'',
+                addPosition:{
+                    did:'',
+                    pname:''
                 }
             };
         },
         mounted() {
-            this.getData();
+            this.getDep();
         },
         computed:{
             nowTableData(){
@@ -116,24 +129,12 @@
                 }).catch(err=>console.log(err));
             },
             getData() {
-                this.$axios2.post('personnel/queryDep').then(res => {
+                console.log(this.deptValue);
+
+                this.$axios2.post('personnel/queryPostionByLike',{did:this.deptValue,pname:this.PositionName}).then(res => {
                     this.tableData = res;
                     if (this.tableData.length % this.query.pageSize==0 &&  this.query.pageIndex!=1){
                         this.$set(this.query, 'pageIndex',this.query.pageIndex-1);
-                    }
-                }).catch(err=>console.log(err));
-            },
-            addDept(){
-                if (this.deptName==""){
-                    this.$message({ duration:2000,message:"名字不能为空",type:"info" });
-                    return;
-                }
-                this.$axios2.post('personnel/addDep.do',{dname:this.deptName}).then(res => {
-                    if (res==1){
-                        this.$message({ duration:2000,message:"添加成功",type:"success" });
-                        this.getData();
-                    }else {
-                        this.$message({ duration:2000,message:"添加失败,可能这个名字已经存在了",type:"warning" });
                     }
                 }).catch(err=>console.log(err));
             },
@@ -143,12 +144,16 @@
             },
             getDep() {
                 this.$axios2.post('personnel/queryDep').then(res => {
-                    this.tableData = res;
-                    if (this.tableData.length % this.query.pageSize==0 &&  this.query.pageIndex!=1){
-                        this.$set(this.query, 'pageIndex',this.query.pageIndex-1);
+                    this.deptData = res;
+                    if (res.length != 0){
+                        this.deptValue = res[0].did;
+                        this.getData();
                     }
                 }).catch(err=>console.log(err));
             }
+        },
+        created() {
+            this.getDep();
         }
     };
 </script>

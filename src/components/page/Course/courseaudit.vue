fde699fb4149e7b14cfcfc5a2cfc76d09ad1d266
huige <huige0404@foxmail.com>
2020-09-08 08:54:39 +0800

    提交1

--- src/components/page/Course/courseaudit.vue
+++ src/components/page/Course/courseaudit.vue
@@ -8,9 +8,6 @@
             </el-breadcrumb>
         </div>
         <div class="container">
-            <div class="handle-box">
-                <el-button type="primary" style="margin-left: 20px;" icon="el-icon-search" @click="setRecomCourse">设置推荐课程</el-button>
-            </div>
             <el-table
                     :data="nowTableData"
                     border
@@ -24,6 +21,21 @@
                 <el-table-column prop="course_type" label="课程类型"></el-table-column>
                 <el-table-column prop="true_name" label="讲师名"></el-table-column>
                 <el-table-column prop="createdate" label="发布时间"></el-table-column>
+                <el-table-column label="课程状态">刚发布</el-table-column>
+                <el-table-column label="操作" width="180" align="center">
+                    <template slot-scope="scope">
+                        <el-button
+                                type="text"
+                                icon="el-icon-edit"
+                                @click="open(scope.row)"
+                        >通过</el-button>
+                        <el-button
+                                type="text"
+                                icon="el-icon-delete"
+                                @click="unopen(scope.row)"
+                        >不通过</el-button>
+                    </template>
+                </el-table-column>
             </el-table>
             <div class="pagination">
                 <el-pagination
@@ -45,11 +57,6 @@
                     header-cell-class-name="table-header"
                     @selection-change="handleSelectionChange"
             >
-<!--                <el-table-column type="selection" width="55" align="center"></el-table-column>-->
-<!--                <el-table-column prop="course_id" label="课程编号" width="55" align="center"></el-table-column>-->
-<!--                <el-table-column prop="course_name" label="课程名"></el-table-column>-->
-<!--                <el-table-column prop="course_type_name" label="课程类型"></el-table-column>-->
-<!--                <el-table-column prop="course_price" label="课程价格"></el-table-column>-->
 
             </el-table>
             <span slot="footer" class="dialog-footer">
@@ -95,7 +102,7 @@
             };
         },
         mounted() {
-            this.getData();
+            this.handlePageChange(1);
         },
         computed:{
             nowTableData(){
@@ -103,6 +110,60 @@
             }
         },
         methods: {
+            //审核通过操作
+            PassCourse(row) {
+                this.$axios2.post('course/passCourse',{course_flag:1,teacherId:row.teacher_id,courseId:row.course_id,approveFlag:1,approver:1}).then(data2=>{
+                    if (data2==1){
+                        this.$message({ duration:1500,message:"操作成功",type:"success" });
+                        this.handlePageChange(1);
+                    }else {
+                        this.$message({ duration:1500,message:"操作失败",type:"warning" });
+                    }
+                    this.handleSearch();
+                }).catch(err=>console.log(err));
+            },
+            //审核不通过操作
+            UnPassCourse(row) {
+                this.$axios2.post('course/passCourse',{course_flag:-1,teacherId:row.teacher_id,courseId:row.course_id,approveFlag:0,approver:1}).then(data2=>{
+                    if (data2==1){
+                        this.$message({ duration:1500,message:"操作成功",type:"success" });
+                        this.handlePageChange(1);
+                    }else {
+                        this.$message({ duration:1500,message:"操作失败",type:"warning" });
+                    }
+                    this.handleSearch();
+                }).catch(err=>console.log(err));
+            },
+            //确认审核是否通过
+            open(row) {
+                this.$confirm('确定通过该课程吗？', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'success'
+                }).then(() => {
+                    this.PassCourse(row);
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消操作'
+                    });
+                });
+            },
+            unopen(row) {
+                this.$confirm('确定不通过该课程吗？', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    this.UnPassCourse(row);
+                    this.getData();
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消操作'
+                    });
+                });
+            },
             setRecomCourse(){
                 this.editVisible = true;
                 this.$axios2.get('course/queryWithCourseTypeAndTea').then(res => {
@@ -135,18 +196,7 @@
                 })
                 this.selectData = JSON.stringify(objfa);
             },
-            // 添加推荐课程操作
-            disableAccount() {
-                this.$axios2.post('TbRecommendCourseController/ForAddCommendCourse',{'commendCourseStr':this.selectData}).then(data2=>{
-                    if(data2 == 1){
-                        this.$message({ duration:1500,message:"推荐成功！",type:"success" })
-                        this.editVisible = false
-                        this.getData()
-                    }else{
-                        this.$message({ duration:1500,message:"操作失败！",type:"warning" })
-                    }
-                }).catch(err=>console.log(err));
-            },
+            //
             // 分页导航
             handlePageChange(val) {
                 this.$set(this.query, 'pageIndex', val);
